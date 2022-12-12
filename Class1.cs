db898037707c4f1089e95d908c4b23d9192bde20
eliza <eliza@DESKTOP-HLLIIPJ>
2022-12-12 18:53:23 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5_uspadkuvannya
+{
+    abstract public class Shape
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void Figure(string Name)
+        {
+            Console.WriteLine($"Це {this.Name}");
+        }
+
+        public abstract double Area();
+        public abstract double Perimeter();
+    }
+
+    public class Circle : Shape
+    {
+        public double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double Area()
+        {
+            double area;
+            area = Math.Round(Math.PI * this.radius, 2);
+            return area;
+        }
+
+        public override double Perimeter()
+        {
+            double perimeter;
+            perimeter = Math.Round(2* Math.PI * this.radius, 2);
+            return perimeter;
+        }
+    }
+
+    public class Square : Shape
+    {
+        private double side;
+        public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public override double Area()
+        {
+            double area;
+            area = Math.Round(4 * this.side, 2);
+            return area;
+        }
+
+        public override double Perimeter()
+        {
+            double perimeter;
+            perimeter = Math.Round(this.side * this.side, 2);
+            return perimeter;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5_uspadkuvannya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Circle circle1 = new Circle(10);
+            Circle circle2 = new Circle(20);
+
+            Console.WriteLine("Площа першого кола = " + circle1.Area());
+            Console.WriteLine("Площа другого кола = " + circle2.Area());
+            Console.WriteLine("Периметр першого кола = " + circle1.Perimeter());
+            Console.WriteLine("Периметр другог кола = " + circle2.Perimeter());
+
+            Square square1 = new Square(10);
+            Square square2 = new Square(20);
+
+            Console.WriteLine("Площа першого квадрату = " + square1.Area());
+            Console.WriteLine("Площа другого квадрату = " + square2.Area());
+            Console.WriteLine("Периметр першого  квадрату = " + square1.Perimeter());
+            Console.WriteLine("Периметр другог квадрату = " + square2.Perimeter());
+
+            if (circle1.Area() > circle2.Area())
+            {
+                Console.WriteLine("Площа найбільшого кола = " + circle1.Area());
+            }
+            else
+            {
+                Console.WriteLine("Площа найбільшого кола = " + circle2.Area());
+            }
+
+            if (square1.Area() > square2.Area())
+            {
+                Console.WriteLine("Площа найбільшого квадрату = " + square1.Area());
+            }
+            else
+            {
+                Console.WriteLine("Площа найбільшого квадрату = " + square2.Area());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
